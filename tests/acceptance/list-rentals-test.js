088a1da2bf52d7ed77a01b4054a37e8f8f7ff622
nhat Hoang <hoangnh092185@gmail.com>
2016-10-23 17:36:56 -0700

    ember-airbnb-rental.

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+let StubMapsService = Ember.Service.extend({
+  getMapElement() {
+    return document.createElement('div');
+  }
+});
+
+moduleForAcceptance('Acceptance | list rentals', {
+  beforeEach() {
+    this.application.register('service:stubMaps', StubMapsService);
+    this.application.inject('component:location-map', 'maps', 'service:stubMaps');
+  }
+});
